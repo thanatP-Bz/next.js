42c14e9e4610a84df0230802af4f1c92786003ed
Thanat <bztzn512@gmail.com>
2023-06-08 18:50:49 -0400

    nextJS

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
+        distinctio omnis ducimus laborum ipsam vel!
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Footer;
