d1542c963b096523be3379da93a167b4bbacefad
Thanat <bztzn512@gmail.com>
2023-06-13 18:01:07 -0400

    next.js

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,7 +1,6 @@
 import React from "react";
+import styles from "./page.module.css";
 
-const Category = () => {
-  return <div>Category</div>;
-};
+const Category = ({ params }) => {};
 
 export default Category;
